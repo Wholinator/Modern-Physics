5c2eeabd40af93ca14742495e74466b8b3d81a8e
Matthew Wholey <s524063@nwmissouri.edu>
2019-02-11 15:18:13 -0600

    List comprehensions to compute over all isotopes of all elements Added labels and title. Finished

--- Nuclear Binding Energy.py
+++ Nuclear Binding Energy.py
@@ -13,9 +13,6 @@ mc2_n_amu = 1.008665
 mc2_h_amu = 1.007825
 
 
-#H = (element('H'))
-#print(H.isotopes[1].mass * 931.5)
-
 def nuclear_mass(A, Z):
     return get_element(A, Z).mass - (Z*mc2_e_amu)
 
@@ -33,6 +30,7 @@ def binding_energy(elem):
 
     return energy_binding
 
+
 def get_element(A, Z):
     #if A != Z :
     #    N = A - Z
@@ -48,18 +46,18 @@ def amu_to_MeV(x):
     return x * 931.5
 
 
-ni_62 = element(28).isotopes[3]
-print(amu_to_MeV(binding_energy(ni_62)) / ni_62.mass)
+x = [element(i).isotopes for i in range(1, 118)]
+# ridiculous list comprehension that flattens  the list of lists of isotopes and removes those without a mass value
+s = [n for i in x for n in i if n.mass is not None]
 
-#ne = element(10).isoto
 
-for i in element(10).isotopes:
-    print(i.mass)
-
-x = [i for i in range(1,100)]
-a = [element(i).isotopes[0].mass for i in x]
-y = [amu_to_MeV(binding_energy(element(i).isotopes[0])) / a[i-1] for i in x]
+a = np.array([i.mass for i in s])
+y = np.array([amu_to_MeV(binding_energy(i)) for i in s])
+y = y/a
 
 
 plt.scatter(a, y)
+plt.xlabel("A")
+plt.ylabel("Nuclear Binding Energy / Nucleon (A)")
+plt.title("Per Nucleon Binding Energy (PNBE) by Number of Nucleons")
 plt.show()
