2924a26b8f4468ef0aeea004e660d5bd13eff143
Matthew Wholey <s524063@nwmissouri.edu>
2019-02-13 16:36:08 -0600

    Added click expansion of label

--- Nuclear Binding Energy.py
+++ Nuclear Binding Energy.py
@@ -55,8 +55,8 @@ s = [n for i in x for n in i if n.mass is not None]
 
 
 a = np.array([i.mass for i in s])
-y = np.array([amu_to_MeV(binding_energy(i)) for i in s])
-y = y/a
+e = np.array([amu_to_MeV(binding_energy(i)) for i in s])
+y = e/a
 
 
 sc = plt.scatter(a, y, s=2)
@@ -77,11 +77,20 @@ annotation = ax.annotate("", xy=(0, 0), xytext=(-20, 20), textcoords="offset poi
 annotation.set_visible(False)
 
 
-def update_annot(ind):
+def update_annot(ind, click):
     annotation.xy = sc.get_offsets()[ind["ind"][0]]
 
+    '''x_, y_ = -20, 20
+    if annotation.xy[0] > 270:
+        x_ = x_ - (40 + (annotation.xy[0] - 270))
+    elif annotation.xy[1] > 7.3:
+        y_ = y_ - (80 + (annotation.xy[1] - 7.3))
+
+    annotation.xytext = (x_, y_)'''
+
     coords = str(ind['ind']).strip('[]').split()
-    iso = str(s[int(coords[0])]).strip()
+    i = int(coords[0])
+    iso = str(s[i]).strip()
 
     Z    = int(iso.split()[0])
     A    = int(iso.split()[1])
@@ -90,10 +99,21 @@ def update_annot(ind):
 
     name = element(Z).name + "-" + str(A) if Z != A else element(Z).name
 
-
-    text = "{}".format(name)
+    if not click:
+        color = (0.99, 0.875, 0.875, 0.9)
+        text = "{}".format(name)
+    else:
+        color = (0.99, 0.825, 0.825, .975)
+        text = "{}\n{}\n{}\n{}\n{}\n{}".format(name,
+                                               "E_be  = " + str(e[i])[:9],
+                                               "PNBE = " + str(y[i])[:9],
+                                               "A = " + str(A) + "      " + "Z = " + str(Z),
+                                               "N = " + str(N),
+                                               "Mass = " + mass)
 
     annotation.set_text(text)
+    annotation.get_bbox_patch().set_facecolor(color)
+    annotation.set_zorder(100)
 
 
 def hover(event):
@@ -101,7 +121,20 @@ def hover(event):
     if event.inaxes == ax:
         cont, ind = sc.contains(event)
         if cont:
-            update_annot(ind)
+            update_annot(ind, False)
+            annotation.set_visible(True)
+            fig.canvas.draw_idle()
+        else:
+            if vis:
+                annotation.set_visible(False)
+                fig.canvas.draw_idle()
+
+def onClick(event):
+    vis = annotation.get_visible()
+    if event.inaxes == ax:
+        cont, ind = sc.contains(event)
+        if cont:
+            update_annot(ind, True)
             annotation.set_visible(True)
             fig.canvas.draw_idle()
         else:
@@ -111,4 +144,5 @@ def hover(event):
 
 
 fig.canvas.mpl_connect("motion_notify_event", hover)
+fig.canvas.mpl_connect("button_press_event", onClick)
 plt.show()
