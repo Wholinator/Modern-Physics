c493f86c0fe81a241d2fe0963e5869666309192b
Matthew Wholey <s524063@nwmissouri.edu>
2019-02-15 12:26:34 -0600

    Changed delta_t to dt

--- Relativity/Relativistic_Graphing.py
+++ Relativity/Relativistic_Graphing.py
@@ -1,16 +1,47 @@
 from tkinter import *
 
+px_per_m = 1
+mass_scaling = 1
 
-def do():
+
+def __init__():
     master = Tk()
 
     canvas_width  = 640
     canvas_height = 400
 
     w = Canvas(master, width=canvas_width, height=canvas_height)
+    w.create_rectangle((0, 0, canvas_width, canvas_height), fill="Black")
     w.pack()
 
-    y = int(canvas_height / 2)
-    w.create_line(0, y, canvas_width, y, fill="#000000")
+    # master.mainloop()
+
+
+def paint(obs_list):
+    # clear all canvas items and refill background
+    w.delete("all")
+    w.create_rectangle((0, 0, canvas_width, canvas_height), fill="Black")
+
+    coords = [convert_coordinates(obs) for obs in obs_list]
+
+    for n in coords:
+        w.create_oval(n, fill="White")
+
+    w.pack()
 
     master.mainloop()
+
+def convert_coordinates(obs):
+    x = obs.x * px_per_m
+    y = obs.y * px_per_m
+
+    size = obs.m0 * mass_scaling
+
+    return x, y, x+size, y+size
+
+# converts between window event coords and canvas coords
+#def callback(event):
+#    canvas = event.widget
+#    x = canvas.canvasx(event.x)
+#    y = canvas.canvasy(event.y)
+#    print(canvas.find_closest(x, y))
--- Relativity/Relativistic_Physics_Engine.py
+++ Relativity/Relativistic_Physics_Engine.py
@@ -1,13 +1,18 @@
 import numpy as np
+from time import perf_counter_ns
 from Relativity import Relativistic_Graphing as Graph
 from Relativity import Classical_Transforms as Classical
 from Relativity import Relativistic_Transforms as Relative
 
+# speed of light
 c = Relative.c
+
+# list of objects
+observers = []
 
 
 class Observer:
-    def __init__(self, x_i, y_i, t_i, m0, vx_i=0, vy_i=0, ax_i=0, ay_i=0, fx_i=0, fy_i=0):
+    def __init__(self, x_i, y_i, m0, t_i=0, vx_i=0, vy_i=0, ax_i=0, ay_i=0, fx_i=0, fy_i=0):
         self.x = x_i
         self.y = y_i
 
@@ -32,7 +37,7 @@ def tick(dt, obs_list):
         fy_i = -0.01
 
         ax_new = Relative.force_to_acceleration(observer.m0, fx_i, observer.vx)
-        ay_new = Relative.force_to_acceleration(observer.mo, fy_i, observer.vy)
+        ay_new = Relative.force_to_acceleration(observer.m0, fy_i, observer.vy)
 
         # verlet scheme
         observer.x = observer.x + (observer.vx * dt) + ((1 / 2) * observer.ax * dt**2)
@@ -41,4 +46,22 @@ def tick(dt, obs_list):
         observer.vx = observer.vx + ((1/2) * (ax_new + observer.ax) * dt)
         observer.vy = observer.vy + ((1/2) * (ay_new + observer.ax) * dt)
 
+        observer.ax = ax_new
+        observer.ay = ay_new
 
+        Graph.paint(obs_list)
+
+
+Graph.__init__()
+
+observers.append(Observer(10, 10, 0, 10))
+observers.append(Observer(20, 30, 0, 10))
+
+t = perf_counter_ns()
+
+while True:
+    t_ = perf_counter_ns()
+
+    tick(t_ - t, observers)
+
+    t = t_
