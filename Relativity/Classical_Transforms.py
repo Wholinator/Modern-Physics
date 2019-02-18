312830872424ba8b0419229c60660f35813ce0dc
Matthew Wholey <s524063@nwmissouri.edu>
2019-02-18 13:50:23 -0600

    Added gravity to Classical_Transforms.py

--- Relativity/Classical_Transforms.py
+++ Relativity/Classical_Transforms.py
@@ -2,7 +2,18 @@
 # Classical Definitions of Physical Transformations #
 #####################################################
 
+import numpy as np
+
+g = 6.67408 * (10**-11)
+
+
 def vel_add(v, u):
     return v + u
 
 
+def gravity(obs1, obs2):
+    return g * (obs1.mass * obs2.mass) / distance(obs1, obs2)
+
+
+def distance(obs1, obs2):
+    return np.sqrt((obs1.x - obs2.x)**2 + (obs1.y - obs2.y)**2)
